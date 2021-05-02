a16cf293134ff2843fbe7705957d9d70b4537bf6
Erin Replogle <erinrep@gmail.com>
2021-05-02 16:11:54 -0600

    heroku config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -14,4 +14,5 @@ secret_key_base =
 
 config :name_that_sprint, NameThatSprintWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  ssl: true,
   secret_key_base: secret_key_base
--- lib/name_that_sprint_web/endpoint.ex
+++ lib/name_that_sprint_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule NameThatSprintWeb.Endpoint do
   ]
 
   socket "/socket", NameThatSprintWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
