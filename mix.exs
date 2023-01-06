4511d2bb0d9ee5ee9cb6350da9b82a928b73cab5
Erin Replogle <erin@Erins-iMac.local>
2023-01-06 14:42:01 -0700

    elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule NameThatSprint.MixProject do
     [
       app: :name_that_sprint,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.14.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
